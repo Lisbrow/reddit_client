b370fd5eb8fa529c5f7c7241c118d7a46d742147
Lisbrow <l.brown91910@gmail.com>
2024-11-09 14:02:26 -0800

    Add no results message to feed

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Post from '../post/Post';
 import { selectFilteredPosts, selectError } from '../../store/redditSlice';
@@ -6,6 +6,15 @@ import { selectFilteredPosts, selectError } from '../../store/redditSlice';
 const Feed = () => {
   const posts = useSelector(selectFilteredPosts);
   const error = useSelector(selectError);
+  const [noResults, setNoResults] = useState(false);
+
+  useEffect(() => {
+    if (posts.length === 0) {
+      setNoResults(true);
+    } else {
+      setNoResults(false);
+    }
+  }, [posts]);
 
   if (error) {
     return <div>Error loading posts. Please try again later.</div>;
@@ -13,6 +22,7 @@ const Feed = () => {
 
   return (
     <div className="Feed">
+      {noResults && <div className="NoResults"><h3>Couldn't find what you are looking for</h3></div>}
       {posts.map((post, index) => (
         <Post key={post.id} post={post} index={index} />
       ))}
